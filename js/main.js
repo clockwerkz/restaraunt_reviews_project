819b65d49e4fa9caf3d93840b39aac1661f7d64f
Carlos Fins <clockwerkz@gmail.com>
2018-09-04 20:39:17 -0700

    adds two types of service workers for the moment

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var markers = []
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', ()=> {
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register('./sw_cached_pages.js')
     .then(reg => console.log('Service Worker: Registered'))
     .catch(err => console.log(`Service Worker Error: ${err}`));
   })
