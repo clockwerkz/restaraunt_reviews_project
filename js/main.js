725c3cbb078c63baa347067d7bc41e00e4c96beb
Carlos <clockwerkz@gmail.com>
2018-09-07 00:08:11 -0700

    edits css to bring main map back, also changes port to use with live server

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ let restaurants,
 var newMap
 var markers = []
 
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', ()=> {
-//     navigator.serviceWorker.register('/sw_cached_pages.js')
-//     .then(reg => console.log('Service Worker: Registered'))
-//     .catch(err => console.log(`Service Worker Error: ${err}`));
-//   })
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', ()=> {
+    navigator.serviceWorker.register('/sw_cached_pages.js')
+    .then(reg => console.log('Service Worker: Registered'))
+    .catch(err => console.log(`Service Worker Error: ${err}`));
+  })
   
-// }
+}
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
